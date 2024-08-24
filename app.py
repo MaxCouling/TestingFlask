5cbb9e7e2d3072607dca3397ccda515ffcecb95e
max <max.couling@outlook.com>
2024-08-24 13:22:55 +1200

    very basic website that works

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Models
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    surveys = db.relationship('Survey', backref='author', lazy=True)
+
+class Survey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+# Routes
+@app.route('/')
+def index():
+    surveys = Survey.query.all()
+    return render_template('index.html', surveys=surveys)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/survey/new', methods=['GET', 'POST'])
+@login_required
+def new_survey():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        survey = Survey(title=title, description=description, author=current_user)
+        db.session.add(survey)
+        db.session.commit()
+        flash('Survey created!', 'success')
+        return redirect(url_for('index'))
+    return render_template('survey.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
